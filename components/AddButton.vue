7f7b6fc72c3fceed925d16f62bfa3eaefd1afd4c
Guillaume <leo.guillaume@epitech.eu>
2021-10-03 21:39:57 +0200

    component add item

---
+++ components/AddButton.vue
@@ -0,0 +1,48 @@
+<template>
+     <div class="new__box" :style="open ? null : 'height: 0; margin: 0'">
+       <input id="text-box" type="text" :placeholder="placeholder"
+              ref="input"
+              @keydown.esc="$emit('onClose')"
+              @input="$emit('input', $event.target.value)"
+              @keypress.enter="$emit('onAdd', value)">
+       <button class="btn btn-primary" style="margin-left: 10px;"
+               @click="handleClick">
+         {{ textButton }}
+       </button>
+       <button class="btn btn-outline-light" style="margin-left: 10px;"
+               @click="$emit('onClose')">Annuler</button>
+     </div>
+</template>
+
+
+<script>
+ export default {
+   name: 'AddButton',
+   props: {
+     placeholder: {type: String, default: ''},
+     textButton: {type: String, default: 'Ajouter'},
+     open: {type: Boolean, default: true},
+     value: {type: String, default: ''}
+   },
+   methods: {
+     handleClick(){
+       this.$emit('onAdd', this.value)
+     }
+   }
+ }
+</script>
+
+<style lang="scss" scoped>
+   .new__box {
+     display: flex;
+     justify-content: space-between;
+     overflow: hidden;
+     height: 39px;
+     margin: 20px 0;
+     transition: all .2s;
+     & input {
+       height: 39px;
+       flex: 1;
+     }
+   }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,20 +6,72 @@
       <b-button @click="updateView('tableList')" expanded>TableList</b-button>
       <b-button @click="updateView('bootstrapButton')" expanded>Custom Boostrap button</b-button>
       <b-button @click="updateView('boostrapDropdown')" expanded>Custom Boostrap dropdown</b-button>
+      <b-button @click="updateView('addButton')" expanded>Add Item Button</b-button>
     </div>
 
     <div class="components" v-if="!showButtons">
       <b-button variant="primary" @click="goBack()">Go back</b-button>
+
       <TimePicker v-if="isVisible.timePicker"/>
+
       <ScrollTimerPicker v-if="isVisible.scrollTimer"/>
+
       <TableList v-model="datas" v-if="isVisible.tableList"/>
+
       <BootstrapButton v-if="isVisible.bootstrapButton"/>
+
       <Dropdown v-if="isVisible.boostrapDropdown"
                 :items="items"
                 color="outline-light"
                 v-model="currentItems">
       </Dropdown>
+
+
+      <div style="display:flex; flex-direction: column; justify-content: center">
+        <div class="mt-3">
+          <button class="btn btn-primary"
+                  @click.prevent="handleOpen('openOne')"
+                  style="margin-left: 10px;">
+            Ouvrir 1
+          </button>
+          valueOne: {{valueOne}}
+        </div>
+
+        <div class="mt-3">
+          <button class="btn btn-primary"
+                  @click.prevent="handleOpen('openTwo')"
+                  style="margin-left: 10px;">
+            Ouvrir 2
+          </button>
+          valueTwo: {{valueTwo}}
+        </div>
+
+        <AddButton
+          ref="inputOne"
+          v-if="isVisible.addButton"
+          v-model="valueOne"
+          :open="openOne"
+          @onClose="closeModal('openOne')"
+          @onAdd="addProject"
+          textButton="Ajouter un projet"
+          placeholder="Enter the name for a new project or task..."
+        />
+
+        <AddButton
+          ref="inputTwo"
+          v-if="isVisible.addButton"
+          v-model="valueTwo"
+          :open="openTwo"
+          @onClose="closeModal('openTwo')"
+          @onAdd="addProject"
+          textButton="Ajouter un projet 2"
+          placeholder="Enter the name for a new project or task..."
+        />
+      </div>
+
+
     </div>
+
   </div>
 
 </template>
@@ -31,6 +83,7 @@ import ScrollTimerPicker from "~/components/ScrollTimerPicker";
 import TableList from "~/components/TableList";
 import BootstrapButton from "~/components/BootstrapButton";
 import Dropdown from "~/components/Dropdown";
+import AddButton from "~/components/AddButton";
 
 
 export default {
@@ -41,16 +94,22 @@ export default {
     ScrollTimerPicker,
     TableList,
     BootstrapButton,
-    Dropdown
+    Dropdown,
+    AddButton
   },
   data() {
     return {
+      openOne: false,
+      valueOne: '',
+      openTwo: true,
+      valueTwo: '',
       isVisible: {
         timePicker: false,
         scrollTimer: false,
         tableList: false,
         bootstrapButton: false,
         boostrapDropdown: false,
+        addButton: false,
       },
       items: ['Active Tasks 1', 'Active Tasks 2', 'Active Tasks 3' ],
       currentItems: 'Active Tasks 1',
@@ -137,6 +196,24 @@ export default {
         this.isVisible[res] = false
       }
     },
+    closeModal(open){
+      if (open === 'openOne'){
+        this.openOne = false;
+      }else if (open === 'openTwo'){
+        this.openTwo = false;
+      }
+    },
+    addProject(val){
+      console.log(val);
+    },
+    handleOpen(open){
+      if (open === 'openOne'){
+        this.openOne = !this.openOne
+        this.$refs.inputOne.$refs.input.focus()
+      }else if (open === 'openTwo'){
+        this.$refs.inputTwo.$refs.input.focus()
+      }
+    }
   }
 }
 </script>
