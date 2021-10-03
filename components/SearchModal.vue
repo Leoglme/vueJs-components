8e995f43b7ac30d447f364c38e9e1ffd469081a5
Guillaume <leo.guillaume@epitech.eu>
2021-10-04 01:39:36 +0200

    search modal component

---
+++ components/SearchModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+    <b-modal
+      v-model="isModalVisible"
+      :modal-class="{'modal-fullscreen': fullscreen}"
+      :hide-backdrop="hideBackdrop"
+      :hide-footer="hideFooter"
+      :hide-header="hideHeader"
+      :hide-header-close="hideClose"
+      :no-close-on-backdrop="noCloseOnBackdrop"
+      :size="size"
+      id="modal-1"
+      title="BootstrapVue">
+      <template #modal-header>
+        <slot name="modal-title"/>
+        <button class="close__modal" @click.prevent="closeModal()">
+          <svg width="20" height="20" viewBox="0 0 20 20">
+            <path d="M10 10l5.09-5.09L10 10l5.09 5.09L10 10zm0 0L4.91 4.91 10 10l-5.09 5.09L10 10z"
+                  stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round"
+                  stroke-linejoin="round"></path>
+          </svg>
+        </button>
+      </template>
+
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'BootstrapModal',
+  data() {
+    return {
+      isModalVisible: false,
+    }
+  },
+  props: {
+    hideBackdrop: {type: Boolean, default: false},
+    hideFooter: {type: Boolean, default: false},
+    hideHeader: {type: Boolean, default: false},
+    hideClose: {type: Boolean, default: false},
+    noCloseOnBackdrop: {type: Boolean, default: false},
+    fullscreen: {type: Boolean, default: false},
+    size: {type: String, default: 'md'},
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    }
+  }
+}
+</script>
+
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
       <b-button @click="updateView('tableList')" expanded>TableList</b-button>
       <b-button @click="updateView('bootstrapButton')" expanded>Custom Boostrap button</b-button>
       <b-button @click="updateView('boostrapDropdown')" expanded>Custom Boostrap dropdown</b-button>
+      <b-button @click="updateView('searchModal')" expanded>Search modal</b-button>
       <b-button @click="updateView('addButton')" expanded>Add Item Button</b-button>
     </div>
 
@@ -27,14 +28,34 @@
       </Dropdown>
 
 
-      <div style="display:flex; flex-direction: column; justify-content: center">
+      <SearchModal v-if="isVisible.searchModal" :hideFooter="true">
+        <template #modal-title>
+
+          <form class="SearchModal__form">
+            <label for="SearchModel__input" class="SearchModel__label" v-if="!isLoading">
+              <svg width="20" height="20" viewBox="0 0 20 20">
+                <path
+                  d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+                  stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round"
+                  stroke-linejoin="round"></path>
+              </svg>
+            </label>
+            <b-spinner style="height: 24px; width: 24px; border-width: 2.5px" variant="primary" v-if="isLoading"/>
+            <input type="search" id="SearchModel__input" placeholder="Search…" class="SearchModel__input">
+          </form>
+
+        </template>
+      </SearchModal>
+
+
+      <div style="display:flex; flex-direction: column; justify-content: center" v-if="isVisible.addButton">
         <div class="mt-3">
           <button class="btn btn-primary"
                   @click.prevent="handleOpen('openOne')"
                   style="margin-left: 10px;">
             Ouvrir 1
           </button>
-          valueOne: {{valueOne}}
+          valueOne: {{ valueOne }}
         </div>
 
         <div class="mt-3">
@@ -43,12 +64,11 @@
                   style="margin-left: 10px;">
             Ouvrir 2
           </button>
-          valueTwo: {{valueTwo}}
+          valueTwo: {{ valueTwo }}
         </div>
 
         <AddButton
           ref="inputOne"
-          v-if="isVisible.addButton"
           v-model="valueOne"
           :open="openOne"
           @onClose="closeModal('openOne')"
@@ -59,7 +79,6 @@
 
         <AddButton
           ref="inputTwo"
-          v-if="isVisible.addButton"
           v-model="valueTwo"
           :open="openTwo"
           @onClose="closeModal('openTwo')"
@@ -84,6 +103,7 @@ import TableList from "~/components/TableList";
 import BootstrapButton from "~/components/BootstrapButton";
 import Dropdown from "~/components/Dropdown";
 import AddButton from "~/components/AddButton";
+import SearchModal from "~/components/SearchModal";
 
 
 export default {
@@ -95,10 +115,12 @@ export default {
     TableList,
     BootstrapButton,
     Dropdown,
-    AddButton
+    AddButton,
+    SearchModal
   },
   data() {
     return {
+      isLoading: false,
       openOne: false,
       valueOne: '',
       openTwo: true,
@@ -110,8 +132,10 @@ export default {
         bootstrapButton: false,
         boostrapDropdown: false,
         addButton: false,
+        bootstrapModal: false,
+        searchModal: false,
       },
-      items: ['Active Tasks 1', 'Active Tasks 2', 'Active Tasks 3' ],
+      items: ['Active Tasks 1', 'Active Tasks 2', 'Active Tasks 3'],
       currentItems: 'Active Tasks 1',
       showButtons: true,
       datas: [
@@ -186,7 +210,7 @@ export default {
       this.isVisible[component] = true
       this.showButtons = false;
     },
-    dropdownAction(name){
+    dropdownAction(name) {
       alert(name)
       return null;
     },
@@ -196,21 +220,21 @@ export default {
         this.isVisible[res] = false
       }
     },
-    closeModal(open){
-      if (open === 'openOne'){
+    closeModal(open) {
+      if (open === 'openOne') {
         this.openOne = false;
-      }else if (open === 'openTwo'){
+      } else if (open === 'openTwo') {
         this.openTwo = false;
       }
     },
-    addProject(val){
+    addProject(val) {
       console.log(val);
     },
-    handleOpen(open){
-      if (open === 'openOne'){
+    handleOpen(open) {
+      if (open === 'openOne') {
         this.openOne = !this.openOne
         this.$refs.inputOne.$refs.input.focus()
-      }else if (open === 'openTwo'){
+      } else if (open === 'openTwo') {
         this.$refs.inputTwo.$refs.input.focus()
       }
     }
