4936d23042d202f73b6b0aefab23ab3eec33e79b
Guillaume <leo.guillaume@epitech.eu>
2021-10-10 02:27:05 +0200

    test ci/cd

--- store/drawerMenu.js
+++ store/drawerMenu.js
@@ -6,5 +6,9 @@ export const state = () => ({
     {icon: null, title: "boot", to: "/drawer-nav"},
     {icon: null, title: "boot", to: "/dropdown"},
     {icon: null, title: "boot", to: "/notification"},
+    {icon: null, title: "boot", to: "/scroll-timePicker"},
+    {icon: null, title: "boot", to: "/search-modal"},
+    {icon: null, title: "boot", to: "/table-list"},
+    {icon: null, title: "boot", to: "/time-picker"},
   ]
 })
